4052f35853478c9b6624083d5cc943c8999990f2
Nidhi Morolia <Nidhi.Morolia@nice.com>
2020-07-13 13:09:03 +0530

    changes for table

--- PerformanceAnalyser/js/performance.js
+++ PerformanceAnalyser/js/performance.js
@@ -1,5 +1,7 @@
 var dataResult=[ { "name": "authentication-manager", "containerCl": 1536, "container": 3072, "jvm": "Xmx=1330m", "number": 4, "baseLineResult.cpu.average": 43, "baseLineResult.cpu.maximum": 66, "baseLineResult.memory.average": 30.2, "baseLineResult.memory.maximum": 32, "baseLineResult.maxUsedHeap": 481, "baseLineResult.maxThread": 271, "currentRunResult.cpu.average": 41.31, "currentRunResult.cpu.maximum": 81.68, "currentRunResult.memory.average": 31.2, "currentRunResult.memory.maximum": 32.36, "currentRunResult.maxUsedHeap": 481.1, "currentRunResult.maxThread": 279 }, {"name": "authorization-manager", "containerCl": 1536, "container": 3072, "jvm": "Xmx=1330m", "number": 4, "baseLineResult.cpu.average": 43, "baseLineResult.cpu.maximum": 66, "baseLineResult.memory.average": 30.2, "baseLineResult.memory.maximum": 32, "baseLineResult.maxUsedHeap": 481, "baseLineResult.maxThread": 271, "currentRunResult.cpu.average": 41.31, "currentRunResult.cpu.maximum": 81.68, "currentRunResult.memory.average": 31.2, "currentRunResult.memory.maximum": 32.36, "currentRunResult.maxUsedHeap": 481.1, "currentRunResult.maxThread": 279 }];
 var columns = ["name", "containerCl", "container", "jvm", "number", "baseLineResult.cpu.average", "baseLineResult.cpu.maximum", "baseLineResult.memory.average", "baseLineResult.memory.maximum", "baseLineResult.maxUsedHeap", "baseLineResult.maxThread", "currentRunResult.cpu.average", "currentRunResult.cpu.maximum", "currentRunResult.memory.average", "currentRunResult.memory.maximum", "currentRunResult.maxUsedHeap", "currentRunResult.maxThread"];
+var componentsResult = []
+
 
 function openContent(event, performance) {
     // Declare all variables
@@ -54,4 +56,37 @@ function loadTable() {
     dataTable.append(table);
 
 }
+
+$("#components").click(function getComponentsDetails(){
+    var table=$("<table/>");
+    table[0].border = "1";
+    var columnCount = 17;
+    var row =$(table[0].insertRow(-1));
+    for (var i=0 ;i<dataResult.length;i++)
+    {
+        row = $(table[0].insertRow(-1));
+        for (var j = 0; j < columnCount; j++) {
+            var cell = $("<td/>");
+            cell.html(dataResult[i][columns[j]]);
+            switch(columns[j]){
+                case "currentRunResult.cpu.average":
+                    if(dataResult[i][columns[j]] < dataResult[i]["baseLineResult.cpu.average"]) {
+                        // cell.style.backgroundColor="red";
+                        cell.addClass("greenClass");
+                    }else if(dataResult[i][columns[j]] > dataResult[i]["baseLineResult.cpu.average"]){
+                        cell.addEventListener("redClass");
+                    }
+                case "baseLineResult.cpu.maximum":
+                    if (dataResult[i][columns[j]]> dataResult[i]["baseLineResult.cpu.maximum"]+10)
+                        cell.addClass("redClass");
+            }
+            row.append(cell);
+        }
+    }
+
+    var dataTable=$("#resultTable");
+    dataTable.html("");
+    dataTable.append(table);
+})
+
 loadTable();
